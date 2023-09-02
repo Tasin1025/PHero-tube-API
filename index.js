6611a52cc8e7af9a358632378225cba17608a89c
Tasin1025 <tasintt@gmail.com>
2023-09-03 00:09:28 +0600

    showing cards

--- index.js
+++ index.js
@@ -22,29 +22,32 @@ const loadVideo = async (categoryid) => {
     const data = await response.json();
 
     const cardContainer = document.getElementById("card-container");
+    cardContainer.innerHTML = "";
 
     data.data.forEach(video => {
-        console.log(video.title);
-    //     const div = document.createElement("div");
-    //     div.innerHTML = `
-    //     <div class="card w-96 bg-base-100 m-5">
-    //     <figure class="px-10 pt-10">
-    //       <img src="https://i.ibb.co/ZNggzdm/cake.jpg" alt="Shoes" class="rounded-xl" />
-    //     </figure>
-    //     <div class="card-body  text-center">
-    //         <div class="flex gap-5 justify-between ">
-    //             <img src="${video.authors[0].profile_picture}" alt="" class="rounded-full h-10 w-10" />
-    //             <h2 class="card-title "> ${video.title}</h2>
-    //         </div>
+        console.log(video.authors[0]);
+        const div = document.createElement("div");
+        div.innerHTML = `
+        <div class="card w-96 bg-base-100 m-5">
+        <figure class="px-10 pt-10">
+          <img src="${video.thumbnail}" alt="Shoes" class="rounded-xl" />
+        </figure>
+        <div class="card-body  text-center">
+            <div class="flex gap-5 justify-between ">
+                <img src="${video.authors[0].profile_picture}" alt="" class="rounded-full h-10 w-10" />
+                <h2 class="card-title "> ${video.title}</h2>
+            </div>
           
-    //     </div>
-    //     <div class="text-left ml-10">
-    //         <p> ${video.authors[0].profile_name} </p>
-    //         <p > views </p>
-    //     </div>   
-    //   </div>
-    //     `;
-    //     cardContainer.appendChild(div);
+        </div>
+        <div class="text-left ml-10">
+            <p> ${video.authors[0].profile_name} </p>
+            <p> ${video.authors[0]?.verified} </p>
+            
+            <p > ${video.others.views} </p>
+        </div>   
+      </div>
+        `;
+        cardContainer.appendChild(div);
     } )
 
 
@@ -53,3 +56,4 @@ const loadVideo = async (categoryid) => {
 };
 
 handleCategory();
+loadVideo("1000");
